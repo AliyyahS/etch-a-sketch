fbbddd3b483f0f0136d9a89dff5527494a4a537a
AliyyahS <50198558+AliyyahS@users.noreply.github.com>
2023-12-08 22:37:46 +0000

    Add slider for changing grid size

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector('.grid');
-const totalHeight = gridContainer.offsetHeight;
+const slider = document.querySelector('#sizeSlider');
+const displaySize = document.querySelector('#displaySize');
 
 function createGrid(size) {
+    const totalHeight = gridContainer.offsetHeight;
     const height = totalHeight/size + 'px';
 
     for (let i = 0; i < size; i++) {
@@ -18,16 +20,37 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+function hoverEffect(colour) {
+    const grid = document.querySelectorAll('.column');
 
-const grid = document.querySelectorAll('.column');
+    grid.forEach(function(div) {
+        div.addEventListener('mouseenter', function () {
+        this.style.backgroundColor = colour;
+        });
 
-grid.forEach(function(div) {
-    div.addEventListener('mouseenter', function () {
-      this.style.backgroundColor = 'pink';
+        div.addEventListener('mouseleave', function () {
+        this.style.backgroundColor = '';
+        });
     });
+}
 
-    div.addEventListener('mouseleave', function () {
-      this.style.backgroundColor = '';
-    });
+function updateGrid(newSize) {
+    const grid = gridContainer.querySelectorAll('div');
+    grid.forEach(div => div.remove());
+    createGrid(newSize);
+    hoverEffect('pink');
+}
+
+// Event listeners
+
+slider.addEventListener('input', function() {
+    displaySize.textContent = `${slider.value} x ${slider.value}`;
+    updateGrid(slider.value);
+});
+
+// On load
+
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid(16);
+    hoverEffect('pink');
 });
